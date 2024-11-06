7c1cfe6a938d6511740bbe342f61442ea7798d76
tmurdoch1 <124635322+tmurdoch1@users.noreply.github.com>
2024-11-06 15:27:40 -0800

    other stuff

--- Project_proposal/phyloseq/phyloseq_object.R
+++ Project_proposal/phyloseq/phyloseq_object.R
@@ -68,7 +68,7 @@ tax_mat <- tax_mat[,-1]
 tax_mat
 
 # Make sampleids the rownames
-rownames(tax_mat) <- taxonomy_data$`Sample Data`
+rownames(tax_mat) <- taxonomy_data$`Feature.ID`
 # Make taxa table
 TAX <- tax_table(tax_mat)
 class(TAX)
@@ -77,5 +77,30 @@ TAX
 #### Create phyloseq object ####
 # Merge all into a phyloseq object
 ms_phyloseq <- phyloseq(OTU, SAMP, TAX, phylo_tree)
-
 OTU 
+
+######### ANALYZE ##########
+# Remove non-bacterial sequences, if any
+ms_filt <- subset_taxa(ms_phyloseq,  Domain == "d__Bacteria" & Class!="c__Chloroplast" & Family !="f__Mitochondria")
+# Remove ASVs that have less than 5 counts total
+ms_filt_nolow <- filter_taxa(ms_filt, function(x) sum(x)>5, prune = TRUE)
+# Remove samples with less than 100 reads
+ms_filt_nolow_samps <- prune_samples(sample_sums(ms_filt_nolow)>100, ms_filt_nolow)
+# Remove samples where month is na
+ms_final <- subset_samples(ms_filt_nolow_samps, !is.na(month) )
+ms_final
+
+# Rarefy samples
+# rngseed sets a random number. If you want to reproduce this exact analysis, you need
+# to set rngseed the same number each time
+# t transposes the table to use rarecurve function
+# cex decreases font size
+rarecurve(t(as.data.frame(otu_table(ms_final))), cex=0.1)
+ms_rare <- rarefy_even_depth(ms_final, rngseed = 1, sample.size = 11257)
+
+
+##### Saving #####
+save(ms_final, file="ms_final.RData")
+save(ms_rare, file="ms_rare.RData")
+
+
