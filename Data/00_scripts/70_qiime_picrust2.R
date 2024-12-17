cf8818f04d21a7bc0822b1134073b37b8e83954f
pammpeng <114960510+pammpeng@users.noreply.github.com>
2024-12-17 11:35:42 -0800

    Update 70_qiime_picrust2.R

--- Data/00_scripts/70_qiime_picrust2.R
+++ Data/00_scripts/70_qiime_picrust2.R
@@ -1,19 +1,21 @@
 ## Picrust 2 QIIME2 Script
 
-
+# Step 1: Filter features in the feature table based on a minimum frequency
 qiime feature-table filter-features \
-  --i-table ms_table.qza \
-  --p-min-frequency 5 \
-  --o-filtered-table feature-frequency-filtered-table.qza
+  --i-table ms_table.qza \  # Input feature table (qiime artifact file), 'ms_table.qza'
+  --p-min-frequency 5 \  # Filters out features with a total frequency less than 5 across all samples
+  --o-filtered-table feature-frequency-filtered-table.qza  # Output filtered feature table, 'feature-frequency-filtered-table.qza'
 
+# Step 2: Run the PICRUSt2 pipeline to predict functional profiles based on the filtered feature table
 qiime picrust2 full-pipeline \
-  --i-table feature-frequency-filtered-table.qza \
-  --i-seq ms_rep-seqs.qza \ 
-  --output-dir q2-picrust2_output \
-  --p-placement-tool sepp \
-  --p-hsp-method pic \
-  --p-max-nsti 2 \
-  --verbose
+  --i-table feature-frequency-filtered-table.qza \  # Input filtered feature table from Step 1, 'feature-frequency-filtered-table.qza'
+  --i-seq ms_rep-seqs.qza \  # Input sequence data (representative sequences), 'ms_rep-seqs.qza'
+  --output-dir q2-picrust2_output \  # Output directory where the results will be saved, 'q2-picrust2_output'
+  --p-placement-tool sepp \  # Placement method for phylogenetic tree building, using SEPP (SATé-enabled phylogenetic placement)
+  --p-hsp-method pic \  # Method for predicting functional genes, using PIC (Phylogenetic Investigation of Communities by Reconstruction of Unobserved States)
+  --p-max-nsti 2 \  # Maximum NSTI (Normalized Strain-Tree Index), the threshold for the reliability of predicted functional genes (higher values indicate less reliable predictions)
+  --verbose  # Outputs additional details during the execution of the pipeline for troubleshooting or further insight
 
-#2 of 2754 ASVs were above the max NSTI cut-off of 2.0 and were removed from the downstream analyses.
+
+#Result said: 2 of 2754 ASVs were above the max NSTI cut-off of 2.0 and were removed from the downstream analyses.
 
