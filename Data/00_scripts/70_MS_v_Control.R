bfbd6e272b9876f8094b518697d7e70ec42ae51d
Asmita Jain <104775028+ajcodde@users.noreply.github.com>
2024-12-17 16:02:24 -0800

    Update 70_MS_v_Control.R
    updated comments and added code to get table with the number of up and downregulated pathways

--- Data/00_scripts/70_MS_v_Control.R
+++ Data/00_scripts/70_MS_v_Control.R
@@ -1,3 +1,5 @@
+# Purpose: The goal of this script is to perform picrust2 functional analysis between the MS v/s control groups
+
 #### Load packages ####
 # Load all necessary libraries
 library(readr)
@@ -17,7 +19,7 @@ abundance_data <- read_delim(abundance_file, delim = "\t", col_names = TRUE, tri
 abundance_data  =as.data.frame(abundance_data) %>% 
   rename('#OTU ID' = 'pathway')
 
-#Import your metadata file, no need to filter yet
+#Importing metadata file
 metadata <- read_delim("ms_metadata.tsv")
 
 # creating a new column with the variables (MS w/ Ashtma, MS w/o asthma, controls)
@@ -27,14 +29,12 @@ metadata <- metadata |>
                              disease_course=='Control' & asthma==0 ~ 'Control',
                              disease_course=='Control' & asthma==1 ~ 'Control_asthma'))
 
-#Example Looking at subject number
-#If you have multiple variants, filter your metadata to include only 2 at a time
 
 #Filtering for variables of interest
 metadata <- metadata %>% 
   filter(disease == c('MS', 'Control'))
 
-#Remove NAs for your column of interest in this case subject
+#Remove NAs for your column of interest in this case disease
 metadata = metadata[!is.na(metadata$disease),]
 
 #Filtering the abundance table to only include samples that are in the filtered metadata
@@ -98,14 +98,22 @@ res_desc = inner_join(res,metacyc_daa_annotated_results_df, by = "feature")
 res_desc = res_desc[, -c(8:13)]
 View(res_desc)
 
-# Filter to only include significant pathways
+# Filter to only include significant pathways and log2fold change
 sig_res = res_desc %>%
   filter(padj < 0.05,
          !between(log2FoldChange,-1.5, 1.5))
-# You can also filter by Log2fold change
 
+# generating data table for the bar plots
 sig_res <- sig_res[order(sig_res$log2FoldChange),]
+
+# generating bar plots for differential expression
 ggplot(data = sig_res, aes(y = reorder(description, sort(as.numeric(log2FoldChange))), x= log2FoldChange, fill = pvalue))+
   geom_bar(stat = "identity")+ 
   theme_bw()+
   labs(x = "Log2FoldChange", y="Pathways")
+
+# code to get the table for the number of upregulated and downregulated pathways:
+sig_res%>% 
+  mutate(expression = ifelse(log2FoldChange < 0, "DOWN", 'UP')) %>% 
+  group_by(expression) %>% 
+  summarise(count = n())
